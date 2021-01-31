0cec2f82d10a825d5349c609bfb8829a5593c2db
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-01-31 17:09:14 +0100

    migrate Issue_2 to Java 15, remove Xtend dependencies

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/Issue_2.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/Issue_2.java
@@ -1,51 +1,54 @@
-package com.trivadis.plsql.formatter.settings.tests
+package com.trivadis.plsql.formatter.settings.tests;
 
-import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter
-import org.junit.Assert
-import org.junit.Test
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.Assert;
+import org.junit.Test;
 
-class Issue_2 extends ConfiguredTestFormatter {
-    
-    val input = '''
+import java.io.IOException;
+
+public class Issue_2 extends ConfiguredTestFormatter {
+    private final String input =
+        """
         select * from dual;
         
         -- test comment
         select * from dual;
-    '''
+        """;
 
-    val expected = '''
+    private final String expected =
+        """
         SELECT *
           FROM dual;
         
         -- test comment
         SELECT *
           FROM dual;
-    '''.toString.trim
-    
-    def String windows(CharSequence input) {
-        input.toString.macos.replace("\n","\r\n")
+        """.trim();
+
+    public String windows(final CharSequence input) {
+        return macos(input.toString()).replace("\n", "\r\n");
     }
-    
-    def String macos(CharSequence input) {
-        return input.toString.replace("\r","")
-    }
-    
-    @Test
-    def macos() {
-        //formatter.options.put(formatter.extraLinesAfterSignificantStatements, Format.BreaksX2.Keep)
-        val actual = formatter.format(input.macos)
-        Assert.assertEquals(expected.macos, actual.macos)
-        val actual2 = formatter.format(actual.macos)
-        Assert.assertEquals(expected.macos, actual2.macos)
+
+    public String macos(final CharSequence input) {
+        return input.toString().replace("\r", "");
     }
 
     @Test
-    def windows() {
+    public void macos() throws IOException {
         //formatter.options.put(formatter.extraLinesAfterSignificantStatements, Format.BreaksX2.Keep)
-        val actual = formatter.format(input.windows)
-        Assert.assertEquals(expected.windows, actual.windows)
-        val actual2 = formatter.format(actual.windows)
-        Assert.assertEquals(expected.windows, actual2.windows)
+        final String actual = getFormatter().format(macos(input));
+        Assert.assertEquals(macos(expected), macos(actual));
+        final String actual2 = getFormatter().format(macos(actual));
+        Assert.assertEquals(macos(expected), macos(actual2));
+    }
+
+    @Test
+    public void windows() throws IOException {
+        //formatter.options.put(formatter.extraLinesAfterSignificantStatements, Format.BreaksX2.Keep)
+        final String actual = getFormatter().format(windows(input));
+        Assert.assertEquals(windows(expected), windows(actual));
+        final String actual2 = getFormatter().format(windows(actual));
+        Assert.assertEquals(windows(expected), windows(actual2));
     }
 
 }
