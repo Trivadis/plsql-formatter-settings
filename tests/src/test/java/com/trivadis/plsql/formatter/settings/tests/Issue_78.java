d3ca69d7cfbfc24e264608f5fe2771f0febf66b0
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-01-31 22:33:36 +0100

    migrate Issue_78 to Java 15, remove Xtend dependencies

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/Issue_78.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/Issue_78.java
@@ -1,14 +1,15 @@
-package com.trivadis.plsql.formatter.settings.tests
+package com.trivadis.plsql.formatter.settings.tests;
 
-import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter
-import oracle.dbtools.app.Format.Breaks
-import org.junit.Test
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import oracle.dbtools.app.Format;
+import org.junit.Test;
 
-class Issue_78 extends ConfiguredTestFormatter {
-    
+public class Issue_78 extends ConfiguredTestFormatter {
+
     @Test
-    def subselect_with_commas_after() {
-        '''
+    public void subselect_with_commas_after() {
+        final String sql = 
+            """
             SELECT table_name,
                    (
                       SELECT COUNT(1)
@@ -17,12 +18,14 @@ class Issue_78 extends ConfiguredTestFormatter {
                    ),
                    t.BLOCKS
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
     @Test
-    def subselect_with_commas_after_with_alias() {
-        '''
+    public void subselect_with_commas_after_with_alias() {
+        final String sql = 
+            """
             SELECT table_name,
                    (
                       SELECT COUNT(1)
@@ -31,14 +34,15 @@ class Issue_78 extends ConfiguredTestFormatter {
                    ) AS num_indexes,
                    t.BLOCKS
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
-
     @Test
-    def subselect_with_commas_before() {
-        formatter.options.put(formatter.breaksComma, Breaks.Before);
-        '''
+    public void subselect_with_commas_before() {
+        getFormatter().options.put(getFormatter().breaksComma, Format.Breaks.Before);
+        final String sql = 
+            """
             SELECT table_name
                  , (
                       SELECT COUNT(1)
@@ -47,13 +51,15 @@ class Issue_78 extends ConfiguredTestFormatter {
                    )
                  , t.BLOCKS
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
     @Test
-    def subselect_with_commas_before_with_alias() {
-        formatter.options.put(formatter.breaksComma, Breaks.Before);
-        '''
+    public void subselect_with_commas_before_with_alias() {
+        getFormatter().options.put(getFormatter().breaksComma, Format.Breaks.Before);
+        final String sql = 
+            """
             SELECT table_name
                  , (
                       SELECT COUNT(1)
@@ -62,13 +68,14 @@ class Issue_78 extends ConfiguredTestFormatter {
                    ) AS num_indexes
                  , t.BLOCKS
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
-
     @Test
-    def first_subselect_with_commas_after() {
-        '''
+    public void first_subselect_with_commas_after() {
+        final String sql = 
+            """
             SELECT (
                       SELECT COUNT(1)
                         FROM user_indexes i
@@ -76,12 +83,14 @@ class Issue_78 extends ConfiguredTestFormatter {
                    ),
                    t.BLOCKS
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
     @Test
-    def first_subselect_with_commas_after_with_alias() {
-        '''
+    public void first_subselect_with_commas_after_with_alias() {
+        final String sql = 
+            """
             SELECT (
                       SELECT COUNT(1)
                         FROM user_indexes i
@@ -89,14 +98,15 @@ class Issue_78 extends ConfiguredTestFormatter {
                    ) AS num_indexes,
                    t.BLOCKS
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
-
     @Test
-    def first_subselect_with_commas_before() {
-        formatter.options.put(formatter.breaksComma, Breaks.Before);
-        '''
+    public void first_subselect_with_commas_before() {
+        getFormatter().options.put(getFormatter().breaksComma, Format.Breaks.Before);
+        final String sql = 
+            """
             SELECT (
                       SELECT COUNT(1)
                         FROM user_indexes i
@@ -104,13 +114,15 @@ class Issue_78 extends ConfiguredTestFormatter {
                    )
                  , t.BLOCKS
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
     @Test
-    def first_subselect_with_commas_before_with_alias() {
-        formatter.options.put(formatter.breaksComma, Breaks.Before);
-        '''
+    public void first_subselect_with_commas_before_with_alias() {
+        getFormatter().options.put(getFormatter().breaksComma, Format.Breaks.Before);
+        final String sql = 
+            """
             SELECT (
                       SELECT COUNT(1)
                         FROM user_indexes i
@@ -118,31 +130,36 @@ class Issue_78 extends ConfiguredTestFormatter {
                    ) AS num_indexes
                  , t.BLOCKS
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
     @Test
-    def subselect_with_single_select_term() {
-        '''
+    public void subselect_with_single_select_term() {
+        final String sql = 
+            """
             SELECT (
                       SELECT COUNT(1)
                         FROM user_indexes i
                        WHERE i.table_name = t.table_name
                    )
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
     @Test
-    def subselect_with_single_select_term_with_alias() {
-        '''
+    public void subselect_with_single_select_term_with_alias() {
+        final String sql = 
+            """
             SELECT (
                       SELECT COUNT(1)
                         FROM user_indexes i
                        WHERE i.table_name = t.table_name
                    ) AS num_indexes
               FROM user_tables t;
-        '''.formatAndAssert
+            """;
+        formatAndAssert(sql);
     }
 
 }
