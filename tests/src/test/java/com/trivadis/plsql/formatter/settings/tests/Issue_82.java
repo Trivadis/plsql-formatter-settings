fc2c83405eda1bec0ee0568e4ab6333b8d47bc85
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-01-31 22:37:01 +0100

    migrate Issue_82 to Java 15, remove Xtend dependencies

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/Issue_82.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/Issue_82.java
@@ -1,45 +1,50 @@
-package com.trivadis.plsql.formatter.settings.tests
+package com.trivadis.plsql.formatter.settings.tests;
 
-import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter
-import org.junit.Test
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.Test;
 
-class Issue_82 extends ConfiguredTestFormatter {
-    
+public class Issue_82 extends ConfiguredTestFormatter {
+
     @Test
-    def bulk_collect_outer_apply() {
-        '''
-        	DECLARE
-        	   l_array my_array_tab;
-        	BEGIN
-        	   SELECT t.a,
-        	          t.b,
-        	          t.c,
-        	          n.stuff
-        	     BULK COLLECT
-        	     INTO l_array
-        	     FROM some_table s
-        	    OUTER APPLY ( s.nested_tab ) n;
-        	END;
-        	/
-        '''.formatAndAssert
+    public void bulk_collect_outer_apply() {
+        final String sql = 
+            """
+            DECLARE
+               l_array my_array_tab;
+            BEGIN
+               SELECT t.a,
+                      t.b,
+                      t.c,
+                      n.stuff
+                 BULK COLLECT
+                 INTO l_array
+                 FROM some_table s
+                OUTER APPLY ( s.nested_tab ) n;
+            END;
+            /
+            """;
+        formatAndAssert(sql);
     }
 
     @Test
-    def bulk_collect_cross_apply() {
-        '''
-        	DECLARE
-        	   l_array my_array_tab;
-        	BEGIN
-        	   SELECT t.a,
-        	          t.b,
-        	          t.c,
-        	          n.stuff
-        	     BULK COLLECT
-        	     INTO l_array
-        	     FROM some_table s
-        	    CROSS APPLY ( s.nested_tab ) n;
-        	END;
-        	/
-        '''.formatAndAssert
+    public void bulk_collect_cross_apply() {
+        final String sql = 
+            """
+            DECLARE
+               l_array my_array_tab;
+            BEGIN
+               SELECT t.a,
+                      t.b,
+                      t.c,
+                      n.stuff
+                 BULK COLLECT
+                 INTO l_array
+                 FROM some_table s
+                CROSS APPLY ( s.nested_tab ) n;
+            END;
+            /
+            """;
+        formatAndAssert(sql);
     }
+
 }
