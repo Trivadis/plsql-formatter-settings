a5bbdb8e046c6bf5ec14e1dec9eaea0c3011b9ca
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-01-31 22:24:48 +0100

    migrate Issue_74 to Java 15, remove Xtend dependencies

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/Issue_74.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/Issue_74.java
@@ -1,12 +1,15 @@
-package com.trivadis.plsql.formatter.settings.tests
+package com.trivadis.plsql.formatter.settings.tests;
 
-import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter
-import oracle.dbtools.app.Format.BreaksX2
-import org.junit.Assert
-import org.junit.Test
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import oracle.dbtools.app.Format;
+import org.junit.Assert;
+import org.junit.Test;
 
-class Issue_74 extends ConfiguredTestFormatter {
-    var input = '''
+import java.io.IOException;
+
+public class Issue_74 extends ConfiguredTestFormatter {
+    private final String input =
+        """
         create or replace package body the_api.math as function to_int_table(in_integers
         in varchar2,in_pattern in varchar2 default '[0-9]+')return sys.ora_mining_number_nt deterministic accessible
         by(package the_api.math,package the_api.test_math)is l_result sys
@@ -15,8 +18,10 @@ class Issue_74 extends ConfiguredTestFormatter {
         exit integer_tokens when l_int is null;l_result.extend;l_result(l_pos):= l_int;l_pos:=l_pos+1;
         end loop integer_tokens;return l_result;end to_int_table;end math;
         /
-    '''
-    var expected = '''
+        """;
+
+    private final String expected =
+        """
         CREATE OR REPLACE PACKAGE BODY the_api.math AS
            FUNCTION to_int_table (
               in_integers  IN  VARCHAR2,
@@ -41,8 +46,10 @@ class Issue_74 extends ConfiguredTestFormatter {
            END to_int_table;
         END math;
         /
-    '''.toString().trim()
-    var expectedX2 = '''
+        """.trim();
+
+    private final String expectedX2 =
+        """
         CREATE OR REPLACE PACKAGE BODY the_api.math AS
         
            FUNCTION to_int_table (
@@ -70,27 +77,27 @@ class Issue_74 extends ConfiguredTestFormatter {
         
         END math;
         /
-    '''.toString().trim()
+        """.trim();
 
     @Test
-    def min_one_line_break_after_with_breaksx2_keep() {
-        formatter.options.put(formatter.extraLinesAfterSignificantStatements, BreaksX2.Keep); // default
-        var actual = formatter.format(input)
-        Assert.assertEquals(expected, actual)
+    public void min_one_line_break_after_with_breaksx2_keep() throws IOException {
+        getFormatter().options.put(getFormatter().extraLinesAfterSignificantStatements, Format.BreaksX2.Keep);
+        String actual = getFormatter().format(input);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    def min_one_line_break_after_with_breaksx2_x1() {
-        formatter.options.put(formatter.extraLinesAfterSignificantStatements, BreaksX2.X1);
-        var actual = formatter.format(input)
-        Assert.assertEquals(expected, actual)
+    public void min_one_line_break_after_with_breaksx2_x1() throws IOException {
+        getFormatter().options.put(getFormatter().extraLinesAfterSignificantStatements, Format.BreaksX2.X1);
+        String actual = getFormatter().format(input);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    def min_one_line_break_after_with_breaksx2_x2() {
-        formatter.options.put(formatter.extraLinesAfterSignificantStatements, BreaksX2.X2);
-        var actual = formatter.format(input)
-        Assert.assertEquals(expectedX2, actual)
+    public void min_one_line_break_after_with_breaksx2_x2() throws IOException {
+        getFormatter().options.put(getFormatter().extraLinesAfterSignificantStatements, Format.BreaksX2.X2);
+        String actual = getFormatter().format(input);
+        Assert.assertEquals(expectedX2, actual);
     }
 
 }
