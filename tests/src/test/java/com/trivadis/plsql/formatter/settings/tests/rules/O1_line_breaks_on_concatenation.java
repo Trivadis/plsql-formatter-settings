4b83a444ab4045e7c473533335f49ec246930e5d
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-05-24 16:51:22 +0200

    refactor O1 test cases, one nested class per option

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/O1_line_breaks_on_concatenation.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/O1_line_breaks_on_concatenation.java
@@ -3,134 +3,169 @@ package com.trivadis.plsql.formatter.settings.tests.rules;
 import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
 import oracle.dbtools.app.Format;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 public class O1_line_breaks_on_concatenation extends ConfiguredTestFormatter {
 
-    @BeforeEach
-    public void setup() {
-        getFormatter().options.put(getFormatter().breaksConcat, Format.Breaks.Before);
+    @Nested
+    class Breaks_Before {
+
+        @BeforeEach
+        public void setup() {
+            getFormatter().options.put(getFormatter().breaksConcat, Format.Breaks.Before);
+        }
+
+        @Test
+        public void add_breaks() throws IOException {
+            var input = """
+                    begin
+                       dbms_output.put_line(
+                          '1' || '2' || '3'
+                       );
+                    end;
+                    """;
+            var actual = formatter.format(input);
+            var expected = """
+                    begin
+                       dbms_output.put_line(
+                          '1'
+                          || '2'
+                          || '3'
+                       );
+                    end;
+                    """;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void change_breaks() throws IOException {
+            var input = """
+                    begin
+                       dbms_output.put_line(
+                          '1' ||
+                          '2' ||
+                          '3'
+                       );
+                    end;
+                    """;
+            var actual = formatter.format(input);
+            var expected = """
+                    begin
+                       dbms_output.put_line(
+                          '1'
+                          || '2'
+                          || '3'
+                       );
+                    end;
+                    """;
+            assertEquals(expected, actual);
+        }
     }
 
-    @Test
-    public void break_before_concat() throws IOException {
-        var input = """
-                begin
-                   dbms_output.put_line(
-                      '1' || '2' || '3'
-                   );
-                end;
-                """;
-        var actual = formatter.format(input);
-        var expected = """
-                begin
-                   dbms_output.put_line(
-                      '1'
-                      || '2'
-                      || '3'
-                   );
-                end;
-                """;
-        assertEquals(expected, actual);
+    @Nested
+    class Breaks_After {
+
+        @BeforeEach
+        public void setup() {
+            getFormatter().options.put(getFormatter().breaksConcat, Format.Breaks.After);
+        }
+
+        @Test
+        public void add_breaks() throws IOException {
+            var input = """
+                    begin
+                       dbms_output.put_line(
+                          '1' || '2' || '3'
+                       );
+                    end;
+                    """;
+            var actual = formatter.format(input);
+            var expected = """
+                    begin
+                       dbms_output.put_line(
+                          '1' ||
+                          '2' ||
+                          '3'
+                       );
+                    end;
+                    """;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void change_breaks() throws IOException {
+            var input = """
+                    begin
+                       dbms_output.put_line(
+                          '1'
+                          || '2'
+                          || '3'
+                       );
+                    end;
+                    """;
+            var actual = formatter.format(input);
+            var expected = """
+                    begin
+                       dbms_output.put_line(
+                          '1' ||
+                          '2' ||
+                          '3'
+                       );
+                    end;
+                    """;
+            assertEquals(expected, actual);
+        }
     }
 
-    @Test
-    public void break_before_concat_with_after() throws IOException {
-        var input = """
-                begin
-                   dbms_output.put_line(
-                      '1' ||
-                      '2' ||
-                      '3'
-                   );
-                end;
-                """;
-        var actual = formatter.format(input);
-        var expected = """
-                begin
-                   dbms_output.put_line(
-                      '1'
-                      || '2'
-                      || '3'
-                   );
-                end;
-                """;
-        assertEquals(expected, actual);
+    @Nested
+    class Breaks_None {
+
+        @BeforeEach
+        public void setup() {
+            getFormatter().options.put(getFormatter().breaksConcat, Format.Breaks.None);
+        }
+
+        @Test
+        public void keep_no_breaks() {
+            var sql = """
+                    begin
+                       dbms_output.put_line(
+                          '1' || '2' || '3'
+                       );
+                    end;
+                    """;
+            formatAndAssert(sql);
+        }
+
+        @Test
+        public void keep_breaks_after() {
+            var sql = """
+                    begin
+                       dbms_output.put_line(
+                          '1' ||
+                          '2' ||
+                          '3'
+                       );
+                    end;
+                    """;
+            formatAndAssert(sql);
+        }
+
+        @Test
+        public void keep_breaks_before() {
+            var sql = """
+                    begin
+                       dbms_output.put_line(
+                          '1'
+                          || '2'
+                          || '3'
+                       );
+                    end;
+                    """;
+            formatAndAssert(sql);
+        }
     }
-
-
-
-    @Test
-    public void break_after_concat() throws IOException {
-        getFormatter().options.put(getFormatter().breaksConcat, Format.Breaks.After);
-        var input = """
-                begin
-                   dbms_output.put_line(
-                      '1' || '2' || '3'
-                   );
-                end;
-                """;
-        var actual = formatter.format(input);
-        var expected = """
-                begin
-                   dbms_output.put_line(
-                      '1' ||
-                      '2' ||
-                      '3'
-                   );
-                end;
-                """;
-        assertEquals(expected, actual);
-    }
-
-
-    @Test
-    public void break_after_concat_with_before() throws IOException {
-        getFormatter().options.put(getFormatter().breaksConcat, Format.Breaks.After);
-        var input = """
-                begin
-                   dbms_output.put_line(
-                      '1'
-                      || '2'
-                      || '3'
-                   );
-                end;
-                """;
-        var actual = formatter.format(input);
-        var expected = """
-                begin
-                   dbms_output.put_line(
-                      '1' ||
-                      '2' ||
-                      '3'
-                   );
-                end;
-                """;
-        assertEquals(expected, actual);
-    }
-
-
-    @Test
-    public void no_break_concat() throws IOException {
-        getFormatter().options.put(getFormatter().breaksConcat, Format.Breaks.None);
-        var input = """
-                begin
-                   dbms_output.put_line(
-                      '1' || '2' || '3'
-                   );
-                end;
-                """;
-        var actual = formatter.format(input);
-        var expected = """
-                begin
-                   dbms_output.put_line(
-                      '1' || '2' || '3'
-                   );
-                end;
-                """;
-        assertEquals(expected, actual);
-    }
-
 }
