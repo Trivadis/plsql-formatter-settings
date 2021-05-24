90c796388cd67a8679d9a3e8aea78a680314d012
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-05-24 14:32:42 +0200

    add test cases for A2: remove trailing spaces

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A2_trailing_spaces.java
@@ -0,0 +1,29 @@
+package com.trivadis.plsql.formatter.settings.tests.rules;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class A2_trailing_spaces extends ConfiguredTestFormatter {
+
+    @Test
+    public void remove_trailing_spaces() throws IOException {
+        final String input =
+            """
+            begin\s\s\s
+            null;\s\s\s\t
+            end;\s\s\s
+            /\s\s\s
+            """;
+        var actual = formatter.format(input);
+        final String expected =
+            """
+            begin
+               null;
+            end;
+            /
+            """;
+        assertEquals(expected, actual);
+    }
+}
