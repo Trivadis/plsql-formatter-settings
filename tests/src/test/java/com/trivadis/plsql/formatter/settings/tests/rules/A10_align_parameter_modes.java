b00eba5d543942a24ca19389d896f3cacf9d07f6
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-05-28 21:15:34 +0200

    add test cases for A10 align parameter modes

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A10_align_parameter_modes.java
@@ -0,0 +1,67 @@
+package com.trivadis.plsql.formatter.settings.tests.rules;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class A10_align_parameter_modes extends ConfiguredTestFormatter {
+
+    @BeforeEach
+    public void setup() {
+        getFormatter().options.put(getFormatter().alignTypeDecl, true);
+        getFormatter().options.put(getFormatter().alignAssignments, true);
+    }
+
+
+    @Test
+    public void procedure() throws IOException {
+        var input = """
+                create procedure p (
+                p1 in integer,
+                p22222 out integer,
+                p333 in out integer,
+                p4 integer
+                ) is
+                begin
+                null;
+                end;
+                /
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                create procedure p (
+                   p1     in     integer,
+                   p22222 out    integer,
+                   p333   in out integer,
+                   p4            integer
+                ) is
+                begin
+                   null;
+                end;
+                /
+                """;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void procedure_single_line() throws IOException {
+        var input = """
+                create procedure p (p1 in integer, p22222 out integer, p333 in out integer, p4 integer) is
+                begin
+                null;
+                end;
+                /
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                create procedure p (p1 in integer, p22222 out integer, p333 in out integer, p4 integer) is
+                begin
+                   null;
+                end;
+                /
+                """;
+        assertEquals(expected, actual);
+    }
+}
