97380b44ab787dc1f827f80b96ddcdd46c832a2f
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-05-28 12:40:51 +0200

    test cases for O7: Align equality predicate

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/O7_align_equality_predicate.java
@@ -0,0 +1,108 @@
+package com.trivadis.plsql.formatter.settings.tests.rules;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class O7_align_equality_predicate extends ConfiguredTestFormatter {
+
+    @Nested
+    class True {
+
+        @BeforeEach
+        public void setup() {
+            getFormatter().options.put(getFormatter().alignEquality, true);
+        }
+
+        @Test
+        public void where() throws IOException {
+            var input = """
+                    select *
+                      from emp
+                     where job = 'CLERK'
+                           and deptno = 10;
+                    """;
+            var actual = formatter.format(input);
+            var expected = """
+                    select *
+                      from emp
+                     where job        = 'CLERK'
+                           and deptno = 10;
+                    """;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void case_expression() throws IOException {
+            var input = """
+                    select ename,
+                    case
+                    when
+                    job = 'CLERK'
+                    and deptno = 10
+                    then
+                    'yes'
+                    else
+                    'no'
+                    end filter_critera
+                    from emp;
+                    """;
+            var actual = formatter.format(input);
+            var expected = """
+                    select ename,
+                           case
+                              when
+                                 job        = 'CLERK'
+                                 and deptno = 10
+                              then
+                                 'yes'
+                              else
+                                 'no'
+                           end filter_critera
+                      from emp;
+                    """;
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Nested
+    class False {
+
+        @BeforeEach
+        public void setup() {
+            getFormatter().options.put(getFormatter().alignTabColAliases, false);
+        }
+
+        @Test
+        public void where() {
+            var sql = """
+                    select *
+                      from emp
+                     where job = 'CLERK'
+                           and deptno = 10;
+                    """;
+            formatAndAssert(sql);
+        }
+
+        @Test
+        public void case_expression() {
+            var sql = """
+                    select ename,
+                           case
+                              when
+                                 job = 'CLERK'
+                                 and deptno = 10
+                              then
+                                 'yes'
+                              else
+                                 'no'
+                           end filter_critera
+                      from emp;
+                    """;
+            formatAndAssert(sql);
+        }
+    }
+}
