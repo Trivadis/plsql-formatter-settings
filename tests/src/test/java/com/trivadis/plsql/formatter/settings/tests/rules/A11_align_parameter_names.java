7a0078173d821bf812e0da640195e95ca9046402
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-05-29 10:03:22 +0200

    add test cases for A11 align parameter names

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A11_align_parameter_names.java
@@ -0,0 +1,65 @@
+package com.trivadis.plsql.formatter.settings.tests.rules;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class A11_align_parameter_names extends ConfiguredTestFormatter {
+
+    @BeforeEach
+    public void setup() {
+        getFormatter().options.put(getFormatter().alignTypeDecl, true);
+        getFormatter().options.put(getFormatter().alignAssignments, true);
+    }
+
+
+    @Test
+    public void procedure() throws IOException {
+        var input = """
+                create procedure p (p1 in integer,
+                p2 in integer,
+                p3 in integer)
+                is
+                begin
+                null;
+                end;
+                /
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                create procedure p (p1 in integer,
+                                    p2 in integer,
+                                    p3 in integer)
+                is
+                begin
+                   null;
+                end;
+                /
+                """;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void procedure_multiline() throws IOException {
+        var input = """
+                create procedure p (p1 in integer, p2 in integer,
+                p3 in integer, p4 in integer) is
+                begin
+                null;
+                end;
+                /
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                create procedure p (p1 in integer, p2 in integer,
+                                    p3 in integer, p4 in integer) is
+                begin
+                   null;
+                end;
+                /
+                """;
+        assertEquals(expected, actual);
+    }
+}
