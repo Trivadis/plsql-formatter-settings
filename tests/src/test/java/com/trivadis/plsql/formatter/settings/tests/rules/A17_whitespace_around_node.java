4a08de58239a85577feb24065c18eaa01bfd804a
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-06-07 13:54:09 +0200

    add A17 test cases to ensure and/or are surrounded by a whitespace

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A17_whitespace_around_node.java
@@ -0,0 +1,44 @@
+package com.trivadis.plsql.formatter.settings.tests.rules;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class A17_whitespace_around_node extends ConfiguredTestFormatter {
+
+    @Test
+    public void and_or_add_spaces() throws IOException {
+        var input = """
+                select 1 from dual where ((1=1)and(2=2)or(3=3));
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                select 1 from dual where ((1 = 1) and (2 = 2) or (3 = 3));
+                """;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void and_or_keep_nl() throws IOException {
+        var input = """
+                select 1 from dual where ((1=1)
+                and
+                (2=2)
+                or
+                (3=3));
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                select 1
+                  from dual
+                 where ((1 = 1)
+                          and
+                          (2 = 2)
+                          or
+                          (3 = 3));
+                """;
+        assertEquals(expected, actual);
+    }
+
+}
