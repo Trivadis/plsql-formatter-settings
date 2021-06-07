61346a6f73e006ce20e70207f9e68656ac7bc866
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-06-07 13:51:09 +0200

    add test cases to reproduce issue #81

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_81_boolean_conditions_and_parenthesis.java
@@ -0,0 +1,65 @@
+package com.trivadis.plsql.formatter.settings.tests.issues;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Issue_81_boolean_conditions_and_parenthesis extends ConfiguredTestFormatter {
+
+    @Test
+    public void sql_condition_single_line() throws IOException {
+        var input = """
+                select * from dba_tables where (owner = 'SYSTEM' and (table_name = 'REDO_DB' or table_name = 'REDO_LOG')) or (owner = 'SYS' and (table_name = 'ALERT_QT' or table_name = 'ALL_UNIFIED_AUDIT_ACTIONS'));
+                """;
+        var expected = """
+                select *
+                  from dba_tables
+                 where (owner = 'SYSTEM' and (table_name = 'REDO_DB' or table_name = 'REDO_LOG'))
+                    or (owner = 'SYS' and (table_name = 'ALERT_QT' or table_name = 'ALL_UNIFIED_AUDIT_ACTIONS'));
+                """;
+        var actual = formatter.format(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void plsql_expr_single_line() throws IOException {
+        var input = """
+                begin if((1=2)or((2=2 and 3=3)or(4=5)))then dbms_output.put_line('Yes');end if;end;
+                """;
+        var expected = """
+                begin
+                   if ((1 = 2) or ((2 = 2 and 3 = 3) or (4 = 5))) then
+                      dbms_output.put_line('Yes');
+                   end if;
+                end;
+                """;
+        var actual = formatter.format(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void plsql_expr_multiline() throws IOException {
+        var input = """
+                begin
+                   if ( ( 1 = 2 ) or ( (
+                      2 = 2 and 3 = 3
+                   ) or ( 4 = 5 ) ) ) then
+                      dbms_output.put_line('Yes');
+                   end if;
+                end;
+                """;
+        var expected = """
+                begin
+                   if ((1 = 2) or ((
+                               2 = 2 and 3 = 3
+                            ) or (4 = 5)))
+                   then
+                      dbms_output.put_line('Yes');
+                   end if;
+                end;
+                """;
+        var actual = formatter.format(input);
+        assertEquals(expected, actual);
+    }
+}
