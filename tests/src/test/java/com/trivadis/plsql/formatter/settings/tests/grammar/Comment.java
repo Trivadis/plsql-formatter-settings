1ceeec66bac150bed73279c0081055838c02f966
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-06-19 15:09:31 +0200

    enable test, not a SQLDev bug, issue in removeDuplicateEmptyLines

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/Comment.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/Comment.java
@@ -1,13 +1,11 @@
 package com.trivadis.plsql.formatter.settings.tests.grammar;
 
 import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class Comment extends ConfiguredTestFormatter {
 
     @Test
-    @Disabled("SQLDev Bug 20.4.1, only one line break delivered by the Lexer after the single line comment!")
     public void one_single_line_with_additional_break() {
         var sql = """
                 -- Single line comment
