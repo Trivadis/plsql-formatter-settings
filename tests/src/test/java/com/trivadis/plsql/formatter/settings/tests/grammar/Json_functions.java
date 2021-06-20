716574e0ab36050b41a438b075177971b7b1fce1
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-06-20 17:13:36 +0200

    add test cases for JSON functions

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/Json_functions.java
@@ -0,0 +1,104 @@
+package com.trivadis.plsql.formatter.settings.tests.grammar;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Json_functions extends ConfiguredTestFormatter {
+
+    @Test
+    public void create_json_doc() {
+        var sql = """
+                select json_serialize(
+                          json_object(
+                             'depts' : json_arrayagg(
+                                json_object(
+                                   'deptno' : d.deptno,
+                                   'dname' : d.dname,
+                                   'loc' : d.loc,
+                                   'emps' :
+                                   (
+                                      select json_arrayagg(
+                                                json_object(
+                                                   'empno' : e.empno,
+                                                   'ename' : e.ename,
+                                                   'job' : e.job,
+                                                   'mgr' : e.mgr,
+                                                   'hiredate' : to_char(e.hiredate, 'YYYY-MM-DD'),
+                                                   'sal' : e.sal,
+                                                   'comm' : e.comm
+                                                   absent on null
+                                                   returning clob
+                                                )
+                                             )
+                                        from emp e
+                                       where e.deptno = d.deptno
+                                   )
+                                   returning clob
+                                )
+                             )
+                             returning clob
+                          )
+                          returning clob pretty
+                       ) as my_json
+                  from dept d;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void json_object_entry_on_same_line() throws IOException {
+        var input = """
+                select json_object(
+                          'deptno'
+                          :
+                          d.deptno,
+                          key
+                          'dname'
+                          value
+                          d.dname,
+                          'loc'
+                          value
+                          d.loc
+                       )
+                  from dept d;
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                select json_object(
+                          'deptno' : d.deptno,
+                          key 'dname' value d.dname,
+                          'loc' value d.loc
+                       )
+                  from dept d;
+                """;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void json_value_on_same_line() throws IOException {
+        var input = """
+                select json_value(
+                          '{firstname:"John"}',
+                          '$.lastname'
+                          default
+                          'No last name found'
+                          on error
+                       ) as "Last Name"
+                  from dual;
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                select json_value(
+                          '{firstname:"John"}',
+                          '$.lastname'
+                          default 'No last name found' on error
+                       ) as "Last Name"
+                  from dual;
+                """;
+        assertEquals(expected, actual);
+    }
+
+
+}
