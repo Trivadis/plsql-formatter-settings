082b32661b99e264b5a8c3f9e934e738b5bf8636
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-06-26 08:02:25 +0200

    add test case for basic loop statement

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/Basic_loop_statement.java
@@ -0,0 +1,39 @@
+package com.trivadis.plsql.formatter.settings.tests.grammar.plsql;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Basic_loop_statement extends ConfiguredTestFormatter {
+
+    @Test
+    public void tokenized() throws IOException {
+        var input = """
+                begin
+                <
+                <
+                infinite_loop
+                >
+                >
+                loop
+                null
+                ;
+                end
+                loop
+                infinite_loop
+                ;
+                end;
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                begin
+                   <<infinite_loop>>
+                   loop
+                      null;
+                   end loop infinite_loop;
+                end;
+                """;
+        assertEquals(expected, actual);
+    }
+}
