ddf508d203d9620be30d69178a43802049f1075e
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-06-27 12:47:14 +0200

    add single line and multi line comments tests

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/Comment.java
@@ -0,0 +1,53 @@
+package com.trivadis.plsql.formatter.settings.tests.grammar.plsql;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Comment extends ConfiguredTestFormatter {
+
+    @Test
+    public void single_line() throws IOException {
+        var input = """
+                begin
+                -- singe line comment
+                null;  -- another single line comment
+                end;
+                /
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                begin
+                   -- singe line comment
+                   null;  -- another single line comment
+                end;
+                /
+                """;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mutli_line() throws IOException {
+        var input = """
+                begin
+                /* multi
+                   line
+                   comment */
+                null  /* another multi line comment */;
+                end;
+                /
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                begin
+                   /* multi 
+                      line 
+                      comment */
+                   null  /* another multi line comment */;
+                end;
+                /
+                """;
+        assertEquals(expected, actual);
+    }
+}
