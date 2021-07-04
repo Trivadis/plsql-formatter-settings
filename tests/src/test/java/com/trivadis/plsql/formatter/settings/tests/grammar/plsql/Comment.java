616014e1aeb3121081ac729349e06dbe09879f68
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-07-04 07:54:20 +0200

    add test cases for comments at the end of an indented structure (loop, if, case)

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/Comment.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/Comment.java
@@ -50,4 +50,91 @@ public class Comment extends ConfiguredTestFormatter {
                 """;
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void within_loop() {
+        var sql = """
+                begin
+                   for i in 1..10
+                   loop
+                      null;
+                      -- single line comment
+                      /* multi line comment */
+                   end loop;
+                end;
+                /
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void within_if() {
+        var sql = """
+                begin
+                   if xyz = 1 then
+                      null;
+                      -- single line comment
+                      /* multi line comment */
+                   elsif xyz = 2 then
+                      null;
+                      -- single line comment
+                      /* multi line comment */
+                   else
+                      null;
+                      -- single line comment
+                      /* multi line comment */
+                   end if;
+                end;
+                /
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void within_simple_case(){
+        var sql = """
+                begin
+                   case xyz
+                      when 1 then
+                         null;
+                         -- single line comment
+                         /* multi line comment */
+                      when 2 then
+                         null;
+                         -- single line comment
+                         /* multi line comment */
+                      else
+                         null;
+                         -- single line comment
+                         /* multi line comment */
+                   end case;
+                end;
+                /
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void within_searched_case(){
+        var sql = """
+                begin
+                   case
+                      when xyz = 1 then
+                         null;
+                         -- single line comment
+                         /* multi line comment */
+                      when xyz = 2 then
+                         null;
+                         -- single line comment
+                         /* multi line comment */
+                      else
+                         null;
+                         -- single line comment
+                         /* multi line comment */
+                   end case;
+                end;
+                /
+                """;
+        formatAndAssert(sql);
+    }
 }
