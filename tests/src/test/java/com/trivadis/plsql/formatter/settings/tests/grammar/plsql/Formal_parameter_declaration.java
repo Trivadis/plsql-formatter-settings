0de230022d32e88a8742458d4cedc96e168a1fea
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-07-23 08:42:11 +0200

    add test case for formal parameter declaration

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/Formal_parameter_declaration.java
@@ -0,0 +1,45 @@
+package com.trivadis.plsql.formatter.settings.tests.grammar.plsql;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Formal_parameter_declaration extends ConfiguredTestFormatter {
+
+    @Test
+    public void all_grammar_variants() throws IOException {
+        var input = """
+                create procedure p (
+                p1 integer,
+                p2 in integer,
+                p3 in number := 3,
+                p4 in integer default 4,
+                p555 out clob,
+                p6 out nocopy clob,
+                p7 in out nocopy clob
+                ) is
+                begin
+                null;
+                end;
+                /
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                create procedure p(
+                   p1                 integer,
+                   p2   in            integer,
+                   p3   in            number  := 3,
+                   p4   in            integer default 4,
+                   p555 out           clob,
+                   p6   out nocopy    clob,
+                   p7   in out nocopy clob
+                ) is
+                begin
+                   null;
+                end;
+                /
+                """;
+        assertEquals(expected, actual);
+    }
+}
