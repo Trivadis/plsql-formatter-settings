46719de5961f9c55ae9f002bb1235a7744fede9e
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-07-25 17:47:19 +0200

    enforce use of GraalVM, use own ScriptRunnerContext, reduce dependencies

--- standalone/src/main/java/com/trivadis/plsql/formatter/TvdFormat.java
+++ standalone/src/main/java/com/trivadis/plsql/formatter/TvdFormat.java
@@ -1,6 +1,7 @@
 package com.trivadis.plsql.formatter;
 
-import oracle.dbtools.raptor.newscriptrunner.*;
+import com.oracle.truffle.js.scriptengine.GraalJSScriptEngine;
+import org.graalvm.polyglot.Context;
 
 import javax.script.*;
 import java.io.*;
@@ -9,44 +10,35 @@ import java.util.function.Predicate;
 import java.util.logging.LogManager;
 
 public class TvdFormat {
-    protected final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-    protected final ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
-    protected final ScriptContext scriptContext = new SimpleScriptContext();
-    protected final ScriptRunnerContext ctx = new ScriptRunnerContext();
-    protected final ScriptExecutor sqlcl = new ScriptExecutor(null);
-    Path tempDir;
+    private ScriptEngine scriptEngine;
+    private ScriptRunnerContext ctx;
 
     TvdFormat() {
-        CommandRegistry.removeListener(SQLCommand.StmtSubType.G_S_FORALLSTMTS_STMTSUBTYPE);
-        CommandRegistry.clearCaches(null, ctx);
-        var bufferedOutputStream = new BufferedOutputStream(System.out);
-        var wrapListenBufferOutputStream = new WrapListenBufferOutputStream(bufferedOutputStream);
-        var bindings = new SimpleBindings();
-        bindings.put("polyglot.js.nashorn-compat", true);
-        bindings.put("polyglot.js.allowHostAccess", Boolean.TRUE);
-        bindings.put("polyglot.js.allowNativeAccess", Boolean.TRUE);
-        bindings.put("polyglot.js.allowCreateThread", Boolean.TRUE);
-        bindings.put("polyglot.js.allowIO", Boolean.TRUE);
-        bindings.put("polyglot.js.allowHostClassLoading", Boolean.TRUE);
-        bindings.put("polyglot.js.allowHostClassLookup", (Predicate<String>) s -> true);
-        bindings.put("polyglot.js.allowAllAccess", Boolean.TRUE);
-        scriptContext.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
-        sqlcl.setOut(bufferedOutputStream);
-        ctx.setOutputStreamWrapper(wrapListenBufferOutputStream);
-        sqlcl.setScriptRunnerContext(ctx);
-        scriptContext.setAttribute("ctx", ctx, ScriptContext.ENGINE_SCOPE);
-        scriptContext.setAttribute("sqlcl", sqlcl, ScriptContext.ENGINE_SCOPE);
+        scriptEngine = GraalJSScriptEngine.create(null,
+                Context.newBuilder("js")
+                .option("js.nashorn-compat", "true")
+                .allowHostAccess(true)
+                .allowNativeAccess(true)
+                .allowCreateThread(true)
+                .allowIO(true)
+                .allowHostClassLoading(true)
+                .allowHostClassLookup((Predicate<String>) s -> true)
+                .allowAllAccess(true));
+        ctx = new ScriptRunnerContext();
+        ctx.setOutputStream(System.out);
+        scriptEngine.getContext().setAttribute("ctx", ctx, ScriptContext.ENGINE_SCOPE);
     }
 
     public void run(String[] arguments) throws IOException, ScriptException {
         LogManager.getLogManager().reset();
+        System.setProperty("tvdformat.standalone", "true");
         var script = Thread.currentThread().getContextClassLoader().getResource("format.js");
         var args = new String[arguments.length + 1];
         args[0] = "format.js";
         System.arraycopy(arguments, 0, args, 1, arguments.length);
-        scriptContext.setAttribute("args", args, ScriptContext.ENGINE_SCOPE);
+        scriptEngine.getContext().setAttribute("args", args, ScriptContext.ENGINE_SCOPE);
         assert script != null;
-        scriptEngine.eval(new InputStreamReader(script.openStream()), scriptContext);
+        scriptEngine.eval(new InputStreamReader(script.openStream()), scriptEngine.getContext());
         ctx.getOutputStream().flush();
     }
 
