5ca89587ab6793946e438fb36e9821bc9c1a1cde
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-07-25 19:03:30 +0200

    change minimal Java version from 11 to 8

--- standalone/src/main/java/com/trivadis/plsql/formatter/TvdFormat.java
+++ standalone/src/main/java/com/trivadis/plsql/formatter/TvdFormat.java
@@ -5,6 +5,7 @@ import org.graalvm.polyglot.Context;
 
 import javax.script.*;
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.function.Predicate;
 import java.util.logging.LogManager;
@@ -31,9 +32,8 @@ public class TvdFormat {
 
     public void run(String[] arguments) throws IOException, ScriptException {
         LogManager.getLogManager().reset();
-        System.setProperty("tvdformat.standalone", "true");
-        var script = Thread.currentThread().getContextClassLoader().getResource("format.js");
-        var args = new String[arguments.length + 1];
+        URL script = Thread.currentThread().getContextClassLoader().getResource("format.js");
+        String[] args = new String[arguments.length + 1];
         args[0] = "format.js";
         System.arraycopy(arguments, 0, args, 1, arguments.length);
         scriptEngine.getContext().setAttribute("args", args, ScriptContext.ENGINE_SCOPE);
@@ -43,7 +43,9 @@ public class TvdFormat {
     }
 
     public static void main(String[] args) throws IOException, ScriptException {
-        var formatter = new TvdFormat();
+        System.setProperty("tvdformat.standalone", "true");
+        System.setProperty("polyglot.engine.WarnInterpreterOnly", "false");
+        TvdFormat formatter = new TvdFormat();
         formatter.run(args);
     }
 }
