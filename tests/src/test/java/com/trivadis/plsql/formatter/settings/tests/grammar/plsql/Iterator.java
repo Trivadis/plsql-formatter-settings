7d1a8c12adf928e6aeb03fd5e0c89a5ce1535a14
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-09-12 13:57:30 +0200

    add tests for indices_of_control, pairs_of_control, cursor_iteration_control

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/Iterator.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/Iterator.java
@@ -138,4 +138,153 @@ public class Iterator extends ConfiguredTestFormatter {
                 """;
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void tokenized_indices_of_control() throws IOException {
+        var input = """
+                declare
+                type t_calling_code_type is table of integer index by varchar2(2);
+                t_calling_codes t_calling_code_type;
+                begin
+                t_calling_codes := t_calling_code_type(
+                'U' || 'S' => 1, 'DE' => 49, 'AT' => 43,
+                'CH' => 41, 'DK' => 45, 'RO' => 40
+                );
+                for
+                i
+                in
+                indices
+                of
+                t_calling_codes
+                when
+                i
+                !
+                =
+                'US'
+                loop
+                dbms_output.put_line('country '
+                || i
+                || ' has country calling code: '
+                || t_calling_codes(i));
+                end loop;
+                end;
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                declare
+                   type t_calling_code_type is table of integer index by varchar2(2);
+                   t_calling_codes t_calling_code_type;
+                begin
+                   t_calling_codes := t_calling_code_type(
+                                         'U' || 'S' => 1, 'DE' => 49, 'AT' => 43,
+                                         'CH' => 41, 'DK' => 45, 'RO' => 40
+                                      );
+                   for i in indices of t_calling_codes when i != 'US'
+                   loop
+                      dbms_output.put_line('country '
+                         || i
+                         || ' has country calling code: '
+                         || t_calling_codes(i));
+                   end loop;
+                end;
+                """;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tokenized_pairs_of_control() throws IOException {
+        var input = """
+                declare
+                type t_calling_code_type is table of integer index by varchar2(2);
+                t_calling_codes t_calling_code_type;
+                begin
+                t_calling_codes := t_calling_code_type(
+                'U' || 'S' => 1, 'DE' => 49, 'AT' => 43,
+                'CH' => 41, 'DK' => 45, 'RO' => 40
+                );
+                for
+                l_index
+                varchar2(2),
+                l_value
+                integer
+                in
+                pairs
+                of
+                t_calling_codes
+                when
+                l_value
+                >
+                1
+                loop
+                dbms_output.put_line('country '
+                || l_index
+                || ' has calling code: '
+                || l_value);
+                end loop;
+                end;
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                declare
+                   type t_calling_code_type is table of integer index by varchar2(2);
+                   t_calling_codes t_calling_code_type;
+                begin
+                   t_calling_codes := t_calling_code_type(
+                                         'U' || 'S' => 1, 'DE' => 49, 'AT' => 43,
+                                         'CH' => 41, 'DK' => 45, 'RO' => 40
+                                      );
+                   for l_index varchar2(2), l_value integer in pairs of t_calling_codes when l_value > 1
+                   loop
+                      dbms_output.put_line('country '
+                         || l_index
+                         || ' has calling code: '
+                         || l_value);
+                   end loop;
+                end;
+                """;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tokenized_cursor_iteration_control() throws IOException {
+        var input = """
+                begin
+                for
+                r
+                in
+                (
+                select
+                ename
+                ,
+                sal
+                from
+                emp
+                )
+                loop
+                dbms_output.put_line('ename '
+                || r.ename
+                || ': '
+                || r.sal);
+                end loop;
+                end;
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                begin
+                   for r in
+                   (
+                      select ename,
+                             sal
+                        from emp
+                   )
+                   loop
+                      dbms_output.put_line('ename '
+                         || r.ename
+                         || ': '
+                         || r.sal);
+                   end loop;
+                end;
+                """;
+        assertEquals(expected, actual);
+    }
 }
