2fbfbdc602360503e2721880e4dd3fc404347938
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-10-07 12:08:53 +0200

    make initializer static

--- tests/src/test/java/com/trivadis/plsql/formatter/sqlcl/tests/AbstractSqlclTest.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/sqlcl/tests/AbstractSqlclTest.java
@@ -13,7 +13,7 @@ import java.util.logging.LogManager;
 import java.util.stream.Collectors;
 
 public abstract class AbstractSqlclTest {
-    {
+    static {
         System.setProperty("polyglot.engine.WarnInterpreterOnly", "false");
     }
 
