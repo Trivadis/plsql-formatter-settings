7ee98dc2581dc4a73f818d1431467879ca017c34
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-10-10 18:05:08 +0200

    remove throws IOException declaration

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_150_define.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_150_define.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 public class Issue_150_define extends ConfiguredTestFormatter {
 
     @BeforeEach
@@ -17,7 +15,7 @@ public class Issue_150_define extends ConfiguredTestFormatter {
 
     @Test
     @Disabled("SQLDev 21.2.1 bug")
-    public void set_define_after_at_command() throws IOException {
+    public void set_define_after_at_command() {
         // in SQLDev 21.2.1 SET is part of the @ command
         // As a result DEFINE becomes a command, and it starts on a new line
         // Same behavior with START as with @.
@@ -31,7 +29,7 @@ public class Issue_150_define extends ConfiguredTestFormatter {
     }
 
     @Test
-    public void commit_after_at_command() throws IOException {
+    public void commit_after_at_command() {
         // workaround
         var sql = """
                 define table_folder = 'table'
