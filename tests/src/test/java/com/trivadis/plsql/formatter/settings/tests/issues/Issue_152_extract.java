a98526b985f8929af93e219145fa9472de67513f
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-10-12 10:16:02 +0200

    remove copied comment

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_152_extract.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_152_extract.java
@@ -7,8 +7,6 @@ public class Issue_152_extract extends ConfiguredTestFormatter {
 
     @Test
     public void extract_from() {
-        // in SQLDev 21.2.1 DECLARE is part of the SPOOL command
-        // As a result there is no way to format that correctly
         var sql = """
                 select e.last_name, e.first_name, d.department_name
                   from employees e
