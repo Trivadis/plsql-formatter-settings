ce4c06e809bac53f957e4917fbde2567162b66d5
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-10-30 17:33:57 +0200

    add test cases for #164 - determine and normalize line separator

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/I10_determine_and_normalize_line_separator.java
@@ -0,0 +1,42 @@
+package com.trivadis.plsql.formatter.settings.tests.rules;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class I10_determine_and_normalize_line_separator extends ConfiguredTestFormatter {
+
+    @Test
+    public void mixed_to_crlf() throws IOException {
+        var input = "select\n*\r\nfrom emp where deptno = 20\norder by sal;";
+        var expected = "select *\r\n  from emp\r\n where deptno = 20\r\n order by sal;";
+        var actual = formatter.format(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lf_only() throws IOException {
+        var input = "select\n*\nfrom emp where deptno = 20\norder by sal;";
+        var expected = "select *\n  from emp\n where deptno = 20\n order by sal;";
+        var actual = formatter.format(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void crlf_only() throws IOException {
+        var input = "select\r\n*\r\nfrom emp where deptno = 20\r\norder by sal;";
+        var expected = "select *\r\n  from emp\r\n where deptno = 20\r\n order by sal;";
+        var actual = formatter.format(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void system_default() throws IOException {
+        var input = "begin null; end;";
+        var expected = "begin" + System.lineSeparator() + "   null;" + System.lineSeparator() + "end;";
+        var actual = formatter.format(input);
+        Assertions.assertEquals(expected, actual);
+    }
+}
