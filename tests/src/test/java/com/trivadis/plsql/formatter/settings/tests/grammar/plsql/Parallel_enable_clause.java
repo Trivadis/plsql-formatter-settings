3ba3a8f672cd10e4c0161a69916115bd5bf9bf82
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-11-02 12:55:27 +0100

    add test cases for parallel enable clause

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/Parallel_enable_clause.java
@@ -0,0 +1,87 @@
+package com.trivadis.plsql.formatter.settings.tests.grammar.plsql;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Parallel_enable_clause extends ConfiguredTestFormatter {
+
+    @Test
+    public void tokenized_parallel_enable() throws IOException {
+        var input = """
+                create
+                function
+                f
+                (
+                in_cursor
+                in
+                sys_refcursor
+                )
+                return
+                some_collection_type
+                pipelined
+                parallel_enable
+                is
+                begin
+                return;
+                end;
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                create function f(
+                   in_cursor in sys_refcursor
+                )
+                   return some_collection_type
+                   pipelined
+                   parallel_enable
+                is
+                begin
+                   return;
+                end;
+                """;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tokenized_parallel_enable_partition_by_any() throws IOException {
+        var input = """
+                create
+                function
+                f
+                (
+                in_cursor
+                in
+                sys_refcursor
+                )
+                return
+                some_collection_type
+                pipelined
+                parallel_enable
+                (
+                partition
+                in_cursor
+                by
+                any
+                )
+                is
+                begin
+                return;
+                end;
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                create function f(
+                   in_cursor in sys_refcursor
+                )
+                   return some_collection_type
+                   pipelined
+                   parallel_enable (partition in_cursor by any)
+                is
+                begin
+                   return;
+                end;
+                """;
+        assertEquals(expected, actual);
+    }
+}
