b661023fb9f0006050668e41e0d2e65a84e80ddc
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-12-14 23:02:47 +0100

    closes #150 - renabled test, issue fixed in SQLDev 21.4.0

--- tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_150_define.java
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_150_define.java
@@ -3,7 +3,6 @@ package com.trivadis.plsql.formatter.settings.tests.issues;
 import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
 import oracle.dbtools.app.Format;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class Issue_150_define extends ConfiguredTestFormatter {
@@ -14,11 +13,11 @@ public class Issue_150_define extends ConfiguredTestFormatter {
     }
 
     @Test
-    @Disabled("SQLDev 21.2.1 bug")
     public void set_define_after_at_command() {
         // in SQLDev 21.2.1 SET is part of the @ command
         // As a result DEFINE becomes a command, and it starts on a new line
         // Same behavior with START as with @.
+        // Fixed in SQLDev 21.4.0.
         var sql = """
                 define table_folder = 'table'
                 set define on
