d8c077d04467a4a9dd090e022328a3faa697b3b3
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-12-19 17:40:06 +0100

    add test case for while loop statement

---
+++ tests/src/test/java/com/trivadis/plsql/formatter/settings/tests/grammar/plsql/While_loop_statement.java
@@ -0,0 +1,78 @@
+package com.trivadis.plsql.formatter.settings.tests.grammar.plsql;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class While_loop_statement extends ConfiguredTestFormatter {
+
+    @Test
+    public void example_14_47_tokenized() throws IOException {
+        var input = """
+                DECLARE
+                  done  BOOLEAN := FALSE;
+                BEGIN
+                  WHILE
+                  done
+                  LOOP
+                  DBMS_OUTPUT
+                  .
+                  PUT_LINE
+                  (
+                  'This line does not print.'
+                  )
+                  ;
+                  done
+                  :
+                  =
+                  TRUE;  -- This assignment is not made.
+                  END
+                  LOOP
+                  ;
+                                
+                  WHILE
+                  NOT
+                  done
+                  LOOP
+                  DBMS_OUTPUT
+                  .
+                  PUT_LINE
+                  (
+                  'Hello, world!'
+                  )
+                  ;
+                  done
+                  :
+                  =
+                  TRUE;
+                  END
+                  LOOP
+                  ;
+                END;
+                /
+                """;
+        var actual = formatter.format(input);
+        var expected = """
+                declare
+                   done boolean := FALSE;
+                begin
+                   while done
+                   loop
+                      DBMS_OUTPUT.PUT_LINE('This line does not print.');
+                      done :=
+                         TRUE;  -- This assignment is not made.
+                   end loop;
+                                
+                   while not done
+                   loop
+                      DBMS_OUTPUT.PUT_LINE('Hello, world!');
+                      done :=
+                         TRUE;
+                   end loop;
+                end;
+                /
+                """;
+        assertEquals(expected, actual);
+    }
+}
