bfe8c8447e0decdbb328e1ff262f3bdabadeb84e
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-12-20 06:43:53 +0100

    using Java 17 text blocks and var

--- standalone/src/test/java/com/trivadis/plsql/formatter/AbstractTvdFormatTest.java
+++ standalone/src/test/java/com/trivadis/plsql/formatter/AbstractTvdFormatTest.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class AbstractTvdFormatTest {
@@ -18,14 +16,14 @@ public abstract class AbstractTvdFormatTest {
     public void setup() {
         try {
             tempDir = Files.createTempDirectory("tvdformat-test-");
-            final URL url = Thread.currentThread().getContextClassLoader().getResource("input");
+            var url = Thread.currentThread().getContextClassLoader().getResource("input");
             assert url != null;
-            final Path resourceDir = Paths.get(url.getPath());
-            final List<Path> sources = Files.walk(resourceDir)
+            var resourceDir = Paths.get(url.getPath());
+            var sources = Files.walk(resourceDir)
                     .filter(Files::isRegularFile)
                     .collect(Collectors.toList());
             for (Path source : sources) {
-                final Path target = Paths.get(tempDir.toString() + File.separator + source.getFileName());
+                var target = Paths.get(tempDir.toString() + File.separator + source.getFileName());
                 Files.copy(source, target);
             }
         } catch (IOException e) {
@@ -42,12 +40,12 @@ public abstract class AbstractTvdFormatTest {
     }
 
     public String getFormattedContent(String fileName) {
-        Path file = Paths.get(tempDir.toString() + File.separator + fileName);
+        var file = Paths.get(tempDir.toString() + File.separator + fileName);
         return getFileContent(file);
     }
 
     public String getExpectedContent(String fileName) {
-        final URL url = Thread.currentThread().getContextClassLoader().getResource("expected");
+        var url = Thread.currentThread().getContextClassLoader().getResource("expected");
         assert url != null;
         return getFileContent(Paths.get(url.getPath() + File.separator + fileName));
     }
--- standalone/src/test/java/com/trivadis/plsql/formatter/TvdFormatTest.java
+++ standalone/src/test/java/com/trivadis/plsql/formatter/TvdFormatTest.java
@@ -7,52 +7,63 @@ import javax.script.ScriptException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TvdFormatTest extends AbstractTvdFormatTest {
 
     @Test
     public void jsonArrayDirTest() throws ScriptException, IOException {
-        final String configFileContent = "[\"" + tempDir.toString() + "\"]";
-        final Path configFile = Paths.get(tempDir + File.separator + "config.json");
+        var configFileContent = """
+                [
+                    "#TEMP_DIR#"
+                ]
+                """.replace("#TEMP_DIR#", tempDir.toString());
+        var configFile = Paths.get(tempDir + File.separator + "config.json");
         Files.write(configFile, configFileContent.getBytes());
-        String[] args = new String[]{tempDir + File.separator + "config.json",
+        var args = new String[]{tempDir + File.separator + "config.json",
                 "xml=" + tempDir + File.separator + "trivadis_advanced_format.xml",
                 "arbori=" + tempDir + File.separator + "trivadis_custom_format.arbori"};
         TvdFormat.main(args);
-        String expected = getExpectedContent("query.sql");
-        String actual = getFormattedContent("query.sql");
+        var expected = getExpectedContent("query.sql");
+        var actual = getFormattedContent("query.sql");
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void jsonArrayFileTest() throws ScriptException, IOException {
-        final String configFileContent = "[\"" + tempDir.toString() + File.separator + "query.sql\"]";
-        final Path configFile = Paths.get(tempDir + File.separator + "config.json");
+        var configFileContent = """
+                [
+                    "#TEMP_DIR##FILE_SEP#query.sql"
+                ]
+                """.replace("#TEMP_DIR#", tempDir.toString()).replace("#FILE_SEP#", File.separator);
+        var configFile = Paths.get(tempDir + File.separator + "config.json");
         Files.write(configFile, configFileContent.getBytes());
-        String[] args = new String[]{tempDir + File.separator + "config.json",
+        var args = new String[]{tempDir + File.separator + "config.json",
                 "xml=" + tempDir + File.separator + "trivadis_advanced_format.xml",
                 "arbori=" + tempDir + File.separator + "trivadis_custom_format.arbori"};
         TvdFormat.main(args);
-        String expected = getExpectedContent("query.sql");
-        String actual = getFormattedContent("query.sql");
+        var expected = getExpectedContent("query.sql");
+        var actual = getFormattedContent("query.sql");
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void jsonObjectFileTest() throws ScriptException, IOException {
-        final String configFileContent = "{\n"
-                + "    \"xml\": \"" + tempDir + File.separator + "trivadis_advanced_format.xml\",\n"
-                + "    \"arbori\": \"" + tempDir + File.separator + "trivadis_custom_format.arbori\",\n"
-                + "    \"files\": [\"" + tempDir.toString() + File.separator + "query.sql\"]\n"
-                + "}";
-        final Path configFile = Paths.get(tempDir + File.separator + "config.json");
+        var configFileContent = """
+                {
+                    "xml": "#TEMP_DIR##FILE_SEP#trivadis_advanced_format.xml",
+                    "arbori": "#TEMP_DIR##FILE_SEP#trivadis_custom_format.arbori",
+                    "files": [
+                        "#TEMP_DIR##FILE_SEP#query.sql"
+                    ]
+                }
+                """.replace("#TEMP_DIR#", tempDir.toString()).replace("#FILE_SEP#", File.separator);
+        var configFile = Paths.get(tempDir + File.separator + "config.json");
         Files.write(configFile, configFileContent.getBytes());
-        String[] args = new String[]{tempDir + File.separator + "config.json"};
+        var args = new String[]{tempDir + File.separator + "config.json"};
         TvdFormat.main(args);
-        String expected = getExpectedContent("query.sql");
-        String actual = getFormattedContent("query.sql");
+        var expected = getExpectedContent("query.sql");
+        var actual = getFormattedContent("query.sql");
         Assertions.assertEquals(expected, actual);
     }
 }
