680e6f4634a64ab76db14f929eb72bc2a0120797
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-12-25 13:29:20 +0100

    add native image Java configuration class called during build

---
+++ standalone/src/main/java/com/trivadis/plsql/formatter/RuntimeReflectionRegistrationFeature.java
@@ -0,0 +1,42 @@
+package com.trivadis.plsql.formatter;
+
+import org.graalvm.nativeimage.hosted.Feature;
+import org.graalvm.nativeimage.hosted.RuntimeReflection;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+
+import java.util.Set;
+
+@SuppressWarnings("unused")
+public class RuntimeReflectionRegistrationFeature implements Feature {
+
+    private static void register(String classNamePrefix, ClassLoader classLoader) {
+        Reflections reflections = new Reflections(classNamePrefix);
+        Set<String> allClassNames = reflections.getAll(Scanners.SubTypes);
+        // allClassNames contains also inner classes
+        for (String className : allClassNames) {
+            registerClass(className, classLoader);
+        }
+    }
+
+    private static void registerClass(String className, ClassLoader classLoader) {
+        try {
+            Class<?> clazz = Class.forName(className,false, classLoader);
+            // calling getClass() on a clazz throws an Exception when not found on the classpath
+            RuntimeReflection.register(clazz.getClass());
+        } catch (Throwable t) {
+            // ignore
+        }
+     }
+
+    public void beforeAnalysis(BeforeAnalysisAccess access) {
+        ClassLoader classLoader = access.getApplicationClassLoader();
+        // register all classes in a package
+        register("oracle.dbtools.app", classLoader);
+        register("oracle.dbtools.arbori", classLoader);
+        register("oracle.dbtools.parser", classLoader);
+        register("oracle.dbtools.raptor.utils", classLoader);
+        register("oracle.dbtools.util", classLoader);
+        register("oracle.dbtools.scripting", classLoader);
+    }
+}
