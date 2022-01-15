324ae2b8fb43ddcf78113380912ede8934cec18d
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-01-15 11:38:30 +0100

    add test cases for #186

---
+++ standalone/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_186_with_clause.java
@@ -0,0 +1,102 @@
+package com.trivadis.plsql.formatter.settings.tests.issues;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import oracle.dbtools.app.Format;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Issue_186_with_clause extends ConfiguredTestFormatter {
+
+    @BeforeEach
+    public void setup_non_trivadis_default_settings() {
+        getFormatter().options.put(getFormatter().idCase, Format.Case.lower);
+    }
+
+    @Test
+    public void keep_single_line() {
+        var sql = """
+                with e as (select * from emp where deptno = 10) select * from e;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void keep_with_on_single_line() {
+        var sql = """
+                with e as (select * from emp where deptno = 10)
+                select *
+                  from e;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void keep_line_break_after_with() {
+        var sql = """
+                with
+                   e as (
+                      select *
+                        from emp
+                       where deptno = 10
+                   )
+                select *
+                  from e;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void keep_single_line_named_queries() {
+        var sql = """
+                with
+                   e as (select * from emp where deptno = 10),
+                   d as (select * from dept)
+                select e.empno, e.name, d.deptno, d.dname
+                  from e
+                  join d
+                    on d.deptno = e.deptno;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void keep_indent_of_plsql_declaration() {
+        var sql = """
+                with
+                   function f(
+                      in_value in number
+                   ) return number is
+                   begin
+                      return in_value;
+                   end;
+                   e as (
+                      select f(empno) as empno,
+                             ename,
+                             deptno
+                        from emp
+                       where deptno = 10
+                   )
+                select *
+                  from e;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void keep_indent_of_plsql_declaration_only() {
+        var sql = """
+                with
+                   function f(
+                      in_value in number
+                   ) return number is
+                   begin
+                      return in_value;
+                   end;
+                select f(empno) as empno,
+                       ename,
+                       deptno
+                  from e;
+                """;
+        formatAndAssert(sql);
+    }
+}
