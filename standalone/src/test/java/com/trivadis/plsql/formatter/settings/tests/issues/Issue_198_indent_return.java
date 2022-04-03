6632bb53bd6bc38048cd0621ad6478c9608f78f2
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-04-03 14:27:40 +0200

    add test case to reproduce #198

---
+++ standalone/src/test/java/com/trivadis/plsql/formatter/settings/tests/issues/Issue_198_indent_return.java
@@ -0,0 +1,94 @@
+package com.trivadis.plsql.formatter.settings.tests.issues;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Issue_198_indent_return extends ConfiguredTestFormatter {
+
+    @Test
+    public void line_break_after_return() throws IOException {
+        var input = """
+                function bool_to_int(in_bool in boolean) return integer
+                      deterministic
+                   is
+                begin
+                   return
+                      case
+                         when in_bool then
+                            1
+                         else
+                            0
+                      end;
+                end bool_to_int;
+                """;
+        var expected = """
+                function bool_to_int(in_bool in boolean) return integer
+                      deterministic
+                   is
+                begin
+                   return case
+                             when in_bool then
+                                1
+                             else
+                                0
+                          end;
+                end bool_to_int;
+                """;
+        var actual = formatter.format(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parenthesis_after_return() {
+        var sql = """
+                function bool_to_int(in_bool in boolean) return integer
+                      deterministic
+                   is
+                begin
+                   return (
+                         case
+                            when in_bool then
+                               1
+                            else
+                               0
+                         end
+                      );
+                end bool_to_int;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void terse_min_max_select() {
+        var sql = """
+                select max(min(case
+                                  when job = 'CLERK' then
+                                     sal
+                                  else
+                                     null
+                               end))
+                  from emp;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void verbose_min_max_select() {
+        var sql = """
+                select max(
+                          min(
+                             case
+                                when job = 'CLERK' then
+                                   sal
+                                else
+                                   null
+                             end
+                          )
+                       )
+                  from emp;
+                """;
+        formatAndAssert(sql);
+    }
+}
