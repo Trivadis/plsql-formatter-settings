aa84fe832ef28db68c9070fa2b83714551d9476a
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-04-03 23:13:27 +0200

    add test case for A24: indent case expression to reproduce #203

---
+++ standalone/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A24_indent_case_expression.java
@@ -0,0 +1,67 @@
+package com.trivadis.plsql.formatter.settings.tests.rules;
+
+import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class A24_indent_case_expression extends ConfiguredTestFormatter {
+
+    @Test
+    public void order_by_case_expr() {
+        var sql = """
+                select o.owner,
+                       o.object_type,
+                       o.object_name
+                  from dba_objects o
+                 where o.owner = coalesce(in_obj.owner, in_parse_user, 'PUBLIC')
+                   and o.object_name = in_obj.object_name
+                 order by case o.owner
+                             when in_obj.owner then
+                                1
+                             when in_parse_user then
+                                2
+                             else
+                                3
+                          end,
+                       case o.object_type
+                          when in_obj.object_type then
+                             1
+                          when 'SYNONYM' then
+                             3
+                          else
+                             2
+                       end;
+                """;
+        formatAndAssert(sql);
+    }
+
+    @Test
+    public void order_by_case_expr_in_parenthesis() {
+        var sql = """
+                select o.owner,
+                       o.object_type,
+                       o.object_name
+                  from dba_objects o
+                 where o.owner = coalesce(in_obj.owner, in_parse_user, 'PUBLIC')
+                   and o.object_name = in_obj.object_name
+                 order by (
+                       case o.owner
+                          when in_obj.owner then
+                             1
+                          when in_parse_user then
+                             2
+                          else
+                             3
+                       end),
+                       case o.object_type
+                          when in_obj.object_type then
+                             1
+                          when 'SYNONYM' then
+                             3
+                          else
+                             2
+                       end;
+                """;
+        formatAndAssert(sql);
+    }
+}
