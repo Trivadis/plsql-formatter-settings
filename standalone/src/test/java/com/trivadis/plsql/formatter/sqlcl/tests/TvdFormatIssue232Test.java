1eb10cae46c7e37cf7e1a7f8423a0ad4a64d680b
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-08-16 00:23:34 +0200

    add test case to reproduce issue #232

---
+++ standalone/src/test/java/com/trivadis/plsql/formatter/sqlcl/tests/TvdFormatIssue232Test.java
@@ -0,0 +1,104 @@
+package com.trivadis.plsql.formatter.sqlcl.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TvdFormatIssue232Test extends AbstractSqlclTest {
+
+    @BeforeEach
+    public void registerCommandBeforeTest() throws IOException {
+        runScript("--register");
+        byteArrayOutputStream.reset();
+        var markdownContent = """
+                # Titel
+                                
+                ## Introduction
+                                
+                This is a Markdown file with some `code blocks`.
+                                
+                ## Indented code block
+                                
+                With version sqlcl-22.2.1 and older the block is extended
+                to the end of the one in the next chapter.
+                                
+                - Bullet point one (don't format)
+                  ``` sql
+                  select
+                  *
+                  from
+                  dual;
+                  ```
+                - Bullet point two (don't format)
+                  ``` sql
+                  select
+                  *
+                  from
+                  dual;
+                  ```
+                                
+                ## sql code block with attributes
+                                
+                A code block to be formatted. Is not formatted in sqlcl-22.2.1.
+                                
+                ```sql title="My SQL block" linenum=10
+                select
+                *
+                from
+                dual;
+                ```
+                """;
+        final Path markdownFile = Paths.get(tempDir + File.separator + "issue-232.md");
+        Files.write(markdownFile, markdownContent.getBytes());
+    }
+
+    @Test
+    public void format_markdown() throws IOException {
+        var actual = runCommand( "tvdformat " + tempDir + File.separator + "issue-232.md");
+        Assertions.assertTrue(actual.contains("issue-232.md"));
+        var actualContent = getFormattedContent("issue-232.md");
+        var expectedContent = """
+                # Titel
+                                
+                ## Introduction
+                                
+                This is a Markdown file with some `code blocks`.
+                                
+                ## Indented code block
+                                
+                With version sqlcl-22.2.1 and older the block is extended
+                to the end of the one in the next chapter.
+                                
+                - Bullet point one (don't format)
+                  ``` sql
+                  select
+                  *
+                  from
+                  dual;
+                  ```
+                - Bullet point two (don't format)
+                  ``` sql
+                  select
+                  *
+                  from
+                  dual;
+                  ```
+                                
+                ## sql code block with attributes
+                                
+                A code block to be formatted. Is not formatted in sqlcl-22.2.1.
+                                
+                ```sql title="My SQL block" linenum=10
+                select *
+                  from dual;
+                ```
+                """;
+        Assertions.assertEquals(expectedContent, actualContent);
+    }
+}
