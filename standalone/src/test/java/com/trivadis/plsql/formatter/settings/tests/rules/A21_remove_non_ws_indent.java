2b70e039dc31b1388766d4adbfd173670ade34b8
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2022-10-02 12:07:46 +0200

    remove unnecessary throws definition

--- standalone/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A21_remove_non_ws_indent.java
+++ standalone/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A21_remove_non_ws_indent.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class A21_remove_non_ws_indent extends ConfiguredTestFormatter {
 
     @Test
-    public void not_applicable() throws IOException {
+    public void not_applicable() {
         // don't know how to write a test case for this Arbori section
         // tested manually by faking original indent to see if the replacement works.
         // e.g. var indent = getIndent(pos) + "aaa bbb\tccc\nddd";
