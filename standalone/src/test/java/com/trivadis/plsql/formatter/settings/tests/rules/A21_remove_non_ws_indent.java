33880fbb32fe32d2265b7805549b9a6cc0b3c313
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2022-10-02 12:10:44 +0200

    delete unnecessary import

--- standalone/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A21_remove_non_ws_indent.java
+++ standalone/src/test/java/com/trivadis/plsql/formatter/settings/tests/rules/A21_remove_non_ws_indent.java
@@ -4,8 +4,6 @@ import com.trivadis.plsql.formatter.settings.ConfiguredTestFormatter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import java.io.IOException;
-
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class A21_remove_non_ws_indent extends ConfiguredTestFormatter {
 
